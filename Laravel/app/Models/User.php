ec2f29e6d251d3b127968a572908d5c34ba279e4
Evan-Duval <duvalevan368@gmail.com>
2025-02-03 15:04:46 +0100

    add user table

--- Laravel/app/Models/User.php
+++ Laravel/app/Models/User.php
@@ -22,9 +22,17 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'username',
+        'birthday',
         'email',
         'password',
+        'rank',
+        'logo',
+        'bio',
+        'certified',
+        'deleted_at',
     ];
 
     /**
