2dc96db707cf52f77c7bf2742843a5545a0d4500
Emix4 <duvalevan368@gmail.com>
2025-02-04 12:00:23 +0100

    add class active for sidebar

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  // Redirection de la route vide vers home
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  // Route pour la page d'accueil
-  { path: 'home', component: HomeComponent }
+  { path: '', redirectTo: 'accueil', pathMatch: 'full' },
+  { path: 'accueil', component: HomeComponent },
+  // Ajoutez vos autres routes ici
+  { path: 'dashboard', component: HomeComponent },
+  { path: 'profil', component: HomeComponent },
+  { path: 'settings', component: HomeComponent }
 ];
