4e389cc6a93edfd04c0b72a3343e22741d90b43a
Emix4 <duvalevan368@gmail.com>
2025-02-04 12:10:32 +0100

    login & register : beginning

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import { Routes } from '@angular/router';
+
+// Auth : 
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+
+// Pages :
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+  // Redirection de base : 
   { path: '', redirectTo: 'accueil', pathMatch: 'full' },
+
+  // Auth :
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+  // Pages :
   { path: 'accueil', component: HomeComponent },
-  // Ajoutez vos autres routes ici
   { path: 'dashboard', component: HomeComponent },
   { path: 'profil', component: HomeComponent },
   { path: 'settings', component: HomeComponent }
