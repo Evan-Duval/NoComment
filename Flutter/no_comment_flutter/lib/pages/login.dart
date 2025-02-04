48994409018d282e131f73d47744b02c2d41da2c
YvertHugo <hugo.yvert@gmail.com>
2025-02-04 14:59:43 +0100

    login style finit

---
+++ Flutter/no_comment_flutter/lib/pages/login.dart
@@ -0,0 +1,32 @@
+import 'package:no_comment_flutter/pages/form_login.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:stroke_text/stroke_text.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF17202A),
+        title: StrokeText(
+          text: 'NoComment',
+          textStyle: GoogleFonts.nanumBrushScript(
+            fontSize: 56,
+            fontWeight: FontWeight.bold,
+            color: const Color(0xFFF6703F), 
+          ),
+          strokeColor: Colors.black,
+          strokeWidth: 6,
+          textAlign: TextAlign.center,
+        ),
+      ),
+      backgroundColor: const Color(0xFF17202A),
+      body: const Center(
+        child: MyCustomForm(),
+      ),
+    );
+  }
+}
